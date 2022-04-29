c21a5cc781f605f0e042da3e3a7a11e70f73961f
NastyaTarasovaa <tarasowaanastaysa@yandex.ru>
2022-04-29 15:35:57 +0300

    add cards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import './components/hero.css';
-import hero from './components/hero';
+import Hero from './components/Hero';
 
 class App extends React.Component {
   render () {
     return (
   <div className="App">
-    <hero></hero>
+    <Hero name="Бэтмен" url="https://n1s1.hsmedia.ru/13/a5/b2/13a5b2373d5e23489d9a4949ada5b927/547x397_0xac120002_8752067681540468870.jpg" text="Род деятельности:борец с преступностью, филантроп"></Hero>
+    <Hero name="Супермен" url="https://n1s1.hsmedia.ru/06/d3/73/06d37321618034ec5f2a65b09c8576e3/547x397_0xac120002_45567661540468871.jpg" text="Род деятельности:борец за справедливость"></Hero>
+    <Hero name="Железный человек" url="https://n1s1.hsmedia.ru/7b/56/08/7b5608ec3df83d872fa1162fb9e32f28/547x397_0xac120002_1773711401540468871.jpg" text="Род деятельности:гений, филантроп"></Hero>
     </div>
   );
 }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,11 +1,11 @@
 import './hero.css';
 
-function hero() {
+function hero(props) {
     return (
         <div className="hero-card" >
-            <img src="https://n1s1.hsmedia.ru/13/a5/b2/13a5b2373d5e23489d9a4949ada5b927/547x397_0xac120002_8752067681540468870.jpg"></img>
-            <div className="hero-name">Бэтмен</div>
-            <div className="hero-text">Род деятельности:борец с преступностью, филантроп</div>
+            <img src={props.url}></img>
+            <div className="hero-name">{props.name}</div>
+            <div className="hero-text">{props.text}</div>
             <button>Оценить</button>
         </div>
     );
